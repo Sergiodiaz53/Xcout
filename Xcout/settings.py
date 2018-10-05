ce73566c5c205e5d3db2b68e37c3ef72a9e7089e
pabrodbra <pabrod@uma.es>
2018-10-05 14:19:28 +0200

    Production working

--- Xcout/settings.py
+++ Xcout/settings.py
@@ -136,14 +136,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_URL = "/static/"
+STATIC_URL = "/xcout/static/"
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 ###
 
 
 # Media files
-MEDIA_URL = '/media/'
+MEDIA_URL = '/xcout/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
